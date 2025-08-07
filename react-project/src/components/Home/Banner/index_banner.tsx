5618180e89eb4884fc32bf01ff2b1e455d713171
victsilva37 <victormau.sm@gmail.com>
2025-08-07 16:02:02 -0400

    Banner responsivo con el código ordenado

--- react-project/src/components/Home/Banner/index_banner.tsx
+++ react-project/src/components/Home/Banner/index_banner.tsx
@@ -10,22 +10,41 @@ import img2_banner from '../../../assets/img/Banner/img2_banner.png';
 export default function Banner() {
     return (
     
-
         <div>
+            {/* Contenedor principal */}
             <div id="banner-container" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+                
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={img1_banner} alt="" className="d-block w-100" />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Analiza tus partidas y mejora el rendimiento de tus partidas de Clash Royale</h5>
+
+                    {/* SECCIÓN PÁGINA 1 CARRUSEL */}
+                        
+                        <div className="carousel-item active">
+
+                            {/*Imagen 1*/}
+                            <img src={img1_banner} alt="" className="d-block w-100" />
+                            
+                            {/*Texto 1*/}
+                            <div className="carousel-caption">
+                                <h5>Analiza tus partidas y mejora el rendimiento de tus partidas de Clash Royale</h5>
+                            </div>
+
                         </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={img2_banner} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Recibe sugerencias inteligentes basadas en tu estilo de juego y resultado recientes</h5>
+
+
+                    {/* SECCIÓN PÁGINA 2 CARRUSEL */}
+
+                        <div className="carousel-item">
+
+                            {/* Imagen 2 */}
+                            <img src={img2_banner} className="d-block w-100" alt="..." />
+                            
+                            {/* Texto 2 */}
+                            <div className="carousel-caption">
+                                <h5>Recibe sugerencias inteligentes basadas en tu estilo de juego y resultado recientes</h5>
+                            </div>
+
                         </div>
-                    </div>
+
                 </div>
  
             </div>
