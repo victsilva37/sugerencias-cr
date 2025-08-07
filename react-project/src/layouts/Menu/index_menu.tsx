e8c6bca10f47b5d6c9a74ce673ca04d7f10a1d15
victsilva37 <victormau.sm@gmail.com>
2025-08-07 15:40:03 -0400

    Se termina el menú

--- react-project/src/layouts/Menu/index_menu.tsx
+++ react-project/src/layouts/Menu/index_menu.tsx
@@ -4,8 +4,14 @@ import logo from '../../assets/img/Menu/logo.png';
 
 export default function Menu() {
   return (
+
+    //Contenedor principal
     <div id="index-menu-container">
-      <img src={logo} alt="" />
+
+      {/* SECCIÓN LOGO APP*/ }
+
+        <img src={logo} alt="" />
+
     </div>
   );
 }
